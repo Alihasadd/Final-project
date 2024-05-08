c87793d193316d195448827c4b706a83c71384c1
Ali Vidler <alisonvidler@gmail.com>
2024-05-08 12:18:32 -0400

    Fixed syntax error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./App.css";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import App1 from './Hodor';
+
 
 function App() {
   return (
     <div>
-      
+      <h1>Portfolio</h1>
+      <App1 />
     </div>
   )
 }
